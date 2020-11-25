c4ab12b220a6c8209435de700b991b8b41573a2d
Yang Liu <lyhello@gmail.com>
2020-11-25 13:00:09 +0000

    version 1.1.0

---
+++ README.md
@@ -0,0 +1,33 @@
+# CME.assistant
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of CME.assistant is to share some frequently used functions
+All the data attached were published thus this package is non-private
+
+## Installation  
+
+To install from github: 
+* Examples in the vignettes, set `build_vignettes = TRUE`
+* May need to add `build_opts` to avoid potential Java problem according to [this post](https://github.com/salimk/Rcrawler/issues/1)
+* Update R and RStudio to the latest version if there are still weird problems
+
+```{r}
+devtools::install_github("unicef-drp/CME.assistant")
+# or if needed,
+devtools::install_github("unicef-drp/CME.assistant", build_vignettes = TRUE, build_opts = "--no-multiarch")
+library(CME.assistant)
+```
+Another alternative: download the repo, and use 'Ctrl+Shift+B' to compile the package locally.
+```{r}
+git clone https://github.com/unicef-drp/CME.assistant.git
+```
+
+## To view vignette
+```{r}
+devtools::install_github("unicef-drp/CME.assistant", build_vignettes = TRUE)
+vignette(package = "CME.assistant")
+vignette("CMEvignette1")
+```
+
---
+++ inst/doc/CMEvignette1.R
@@ -0,0 +1,38 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## -----------------------------------------------------------------------------
+library(CME.assistant)
+
+## ---- eval = TRUE-------------------------------------------------------------
+# without supplying dir_file, by default returns the published IGME 2020 final
+# results
+dt_1 <- get.CME.UI.data(use_IGME_year = 2020)
+head(dt_1)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  # can read final output file from Dropbox for any indicator
+#  # loading Dropbox files requires access to corresponding files
+#  dir_list <- load.final_dir()
+#  dt_2 <- get.CME.UI.data(dir_file = dir_list$dir_female_2020)
+#  head(dt_2)
+
+## ---- eval = FALSE------------------------------------------------------------
+#  input_dirs <- load.IGMEinput.dir()
+#  dir_U5MR   <- get.dir_U5MR(input_dirs$dir_IGME_20)
+#  dir_IMR    <- get.dir_IMR(input_dirs$dir_IGME_20)
+#  dir_NMR    <- get.dir_NMR(y5 = TRUE) # either 5-year or not
+#  dir_gender <- get.dir_gender(plotting = TRUE) # either dataset for plotting or modeling
+
+## -----------------------------------------------------------------------------
+# a helpful function to revise variable, you can provide a __new_list__ to define the labels
+get.match(c("a","b", "c"), new_list = list("a" = "new label 1",
+                                           "b" = "new label 2"))
+
+
+## -----------------------------------------------------------------------------
+str(UNICEF_colors)
+
